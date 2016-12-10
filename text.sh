e52df47bccd8d11e22d93dcf23840c4249f602ce
Haifan <haifan.wu@ucsf.edu>
2016-12-10 10:33:03 -0800

    first

---
+++ text.sh
@@ -0,0 +1,22 @@
+echo "starting"
+
+for file in *.txt
+do
+
+echo "$file"
+cat header.txt "$file" > processed/$file
+
+done
+
+cd processed
+
+for input in *.txt
+do
+
+python 1.py $input
+
+done
+
+echo "Done!"
+
+ 
